5743ae55ee42a06f7cfd8c488a9d202e670ab7ba
Taylor Hessel <taylorhessel@gmail.com>
2017-08-26 16:56:32 -0600

    Added basic Spring Security to playground application

--- src/test/java/com/example/demo/FlightsControllerTests.java
+++ src/test/java/com/example/demo/FlightsControllerTests.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(FlightsController.class)
+@AutoConfigureMockMvc(secure = false)
 public class FlightsControllerTests {
 
     @Autowired
--- src/test/java/com/example/demo/WordsControllerWebMvcTests.java
+++ src/test/java/com/example/demo/WordsControllerWebMvcTests.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -22,6 +23,7 @@ import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(WordsController.class)
+@AutoConfigureMockMvc(secure = false)
 public class WordsControllerWebMvcTests {
 
     @Autowired
